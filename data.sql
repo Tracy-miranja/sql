310bfc907bf1155bb11860a26461cef90c69c32f
Tracy-miranja <barazatracy44@gmail.com>
2023-05-09 15:42:28 +0300

    Update files

--- data.sql
+++ data.sql
@@ -1,5 +1,3 @@
-/* Populate database with sample data. */
-
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg ) VALUES (1,'Agumon','2020-02-03',0, 'true', 10.23);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (2,'Gabumon','2018-11-15',2, 'true', 8);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (3,' Pikachu','2021-01-07',1, 'false', 15.04,);
