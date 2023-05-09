70eb35c1496c315c2115f32a2c45dba3c815b879
Tracy Miranja <barazatracy44@gmail.com>
2023-05-09 13:49:44 +0300

    Update schema.sql
    Copy table created to the schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id int PRIMARY KEY,
+  name VARCHAR(255) NOT null,
+  date_of_birth DATE NOT null,
+  escape_attempts int null,
+  neutered  boolean null,
+  neutered_weight_kg decimal NOT null
 );
+
