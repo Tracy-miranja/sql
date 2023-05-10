73a144c27938bf68b61bbca05c980e151d573511
Tracy-miranja <barazatracy44@gmail.com>
2023-05-10 22:33:03 +0300

    Add owner table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,14 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
