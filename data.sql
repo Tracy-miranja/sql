0420f89f159cef34d5f6e3023a3a91337b801b9a
Tracy-miranja <barazatracy44@gmail.com>
2023-05-10 11:15:50 +0300

    add data to data.sql file

--- data.sql
+++ data.sql
@@ -2,3 +2,10 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutere
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (2,'Gabumon','2018-11-15',2, 'true', 8);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (3,' Pikachu','2021-01-07',1, 'false', 15.04,);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Devimon','2017-05-12',5, 'true', 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Charmander','2020-02-08',0, 'false', 8);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,' Plantmon','2021-11-15',2, 'true', 5.7);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Squirtle','1993-04-02',3, 'false', 12.13);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Angemon','2003-06-12',1, 'true', 45);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Boarmon','2005-06-07',7, 'true', 20.40);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Blossom','1998-10-13',3, 'true', 17);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, neutered_weight_kg) VALUES (4,'Ditto','2022-05-14',4, 'true', 22);
--- schema.sql
+++ schema.sql
@@ -4,5 +4,8 @@ CREATE TABLE animals (
   date_of_birth DATE NOT null,
   escape_attempts int null,
   neutered  boolean null,
-  neutered_weight_kg decimal NOT null
+  neutered_weight_kg decimal NOT null,
+  species VARCHAR(255)
 );
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
