e8c0f2852699bcf42b1d3baefb27924078691e88
Peter Okorafor <peterdtitan@gmail.com>
2023-05-17 14:05:21 +0200

    Create Table and Add Indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -41,5 +41,18 @@ CREATE TABLE invoice_items(
     constraint fk_invoice_items_treatment foreign key(treatment_id) references treatment(id)
 )
 
+ create table medical_treatment (
+    id int primary key generated always as  identity,
+    medical_history_id int,
+    treatment_id,
+    constraint fk_medical_treatment_history foreign key(medical_history_id) references medical_histories(id),
+    constraint fk_medical_treatment_treatment foreign key(treatment_id) references treatment(id)
+);
 
+--------INDEX-------------
+CREATE index medical_treatment_medical_treatment_history_idx ON medical_treatment(medical_treatment_history asc);
+CREATE index medical_treatment_medical_treatment_treatment_idx ON medical_treatment(medical_treatment asc);
+CREATE index invoice_items_invoice_items_invoice_idx ON invoice_items(invoice_items_invoice);
+CREATE index invoice_items_invoice_items_treatment_idx ON invoice_items(invoice_items_treatment,DESC);
+CREATE index invoices_invoices_medical_history_idx ON invoices(invoices_medical_history,ASC);
 
